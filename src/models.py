e223db9eb9a7f7ec75302494380f37cbd6ac6626
Walt Castillo <walt.castillo@gmail.com>
2021-03-25 02:42:51 +0000

    instagram

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,56 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class login(Base):
+    __tablename__ = 'login'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(30))
+    first_name = Column(String(30))
+    second_name = Column(String(30))
+    password = Column(String(30))
+    password_confirmation = Column(String(30))
+    state = Column(Boolean, default=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class user(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nickname = Column(String(250))
+    email = Column(String(250))
+    phone = Column(String(250), nullable=False)
+    state = Column(Integer, ForeignKey('login.state'))
+    username = Column(Integer, ForeignKey('login.username'))
+    password = Column(Integer, ForeignKey('login.password'))
+    login = relationship(login)
+
+class likes(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    n_likes = Column(Integer)
+    username = Column(String(250))
+    nickname = Column(String(250))
+    post_id = Column(Integer, ForeignKey("post.id"))
+    follow = Column(Boolean, default=True)
+
+class post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    photo = Column(String(250))
+    video = Column(String(250))
+    url = Column(String(250))
+    text = Column(String(250))
+    comments = Column(String(250))
+    n_likes = Column(Integer)
+    likes = relationship(likes)
+
+class feed(Base):
+    __tablename__ = 'feed'
+    id = Column(Integer, primary_key=True)
+    n_posts = Column(String(250))
+    followers = Column(String(250))
+    post_id = Column(Integer, ForeignKey("post.id"))
+    user =relationship(user)
 
     def to_dict(self):
         return {}
